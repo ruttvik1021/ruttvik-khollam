e59e26c013020a392b82afc231e2d9bed532ed9a
Ruttvik21 <rkhollam21@live.com>
2024-11-01 00:55:05 +0530

    updated the toggle button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -59,34 +59,43 @@ enum Modes {
 }
 
 const ThemeToggleButton = () => {
-  const getStoredTheme = (localStorage.getItem(Theme) as Modes) || null;
-  const [activeTheme, setActiveTheme] = useState<Modes | null>(getStoredTheme);
-  const htmlElement = document.documentElement;
+  const [activeTheme, setActiveTheme] = useState<Modes | null>(null);
 
   useEffect(() => {
-    if (activeTheme) {
-      toggleTheme(activeTheme);
+    if (typeof window !== "undefined") {
+      // Check if window is defined
+      const storedTheme = (localStorage.getItem(Theme) as Modes) || Modes.LIGHT;
+      setActiveTheme(storedTheme);
+      toggleTheme(storedTheme); // Apply the stored theme
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [activeTheme]);
+  }, []);
 
   const toggleTheme = (theme: Modes) => {
-    localStorage.setItem(Theme, theme);
-    htmlElement.classList.add(theme);
+    if (typeof window !== "undefined") {
+      localStorage.setItem(Theme, theme);
+      document.documentElement.classList.toggle(
+        Modes.DARK,
+        theme === Modes.DARK
+      );
+      document.documentElement.classList.toggle(
+        Modes.LIGHT,
+        theme === Modes.LIGHT
+      );
+    }
   };
 
   return activeTheme === Modes.DARK ? (
     <Sun
       onClick={() => {
         setActiveTheme(Modes.LIGHT);
-        htmlElement.classList.remove(Modes.DARK);
+        toggleTheme(Modes.LIGHT);
       }}
     />
   ) : (
     <Moon
       onClick={() => {
         setActiveTheme(Modes.DARK);
-        htmlElement.classList.remove(Modes.LIGHT);
+        toggleTheme(Modes.DARK);
       }}
     />
   );
